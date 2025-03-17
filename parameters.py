583bb91c6cec3fcb300518cc2b5f6b1487c7434b
dell <lhlphy@qq.com>
2025-03-18 02:28:33 +0800

    Kepler-78 b

--- parameters.py
+++ parameters.py
@@ -26,7 +26,7 @@ class Planet_parameters:
         self.Tss = self.Stellar_T / np.sqrt(self.semi_axis / self.Rs)
         self.Rp2Rs = self.Rp / self.Rs
         print("alpha: ", self.Rs / self.semi_axis)
-        self.Coefficents = [0.403, 0.256]
+        self.Coefficents = [0.519, 0.178]
 
 
 # PPs = Planet_parameters(4170) # K2-141 b
