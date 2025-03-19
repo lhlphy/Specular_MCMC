a5685e519577840603265f5fe046b330d5edb091
dell <lhlphy@qq.com>
2025-03-19 14:23:33 +0800

    final test

--- core_lambert/test.py
+++ core_lambert/test.py
@@ -29,16 +29,18 @@ if __name__ == '__main__':
     F = 0
     Tss = PPs.Tss
     Rp2Rs = PPs.Rp2Rs
-    inc = 80
+    inc = 60
     Coefficents = PPs.Coefficents
     print("Tss standard is: ", Tss)
     print("Rp/Rs standard is: ", Rp2Rs)
     
     # plot each function
-    F_thermal = F_thermal(Theta_array, AB, F, Tss, Rp2Rs, inc)
-    F_specular = F_lambert(Theta_array, AB, Rp2Rs, inc)
-    F_Doppler = F_Doppler(Theta_array, alpha_Doppler)
-    F_ellip = F_ellip(Theta_array, alpha_ellipse)
+    F_thermal1 = F_thermal(Theta_array, AB, F, Tss, Rp2Rs, inc)
+    F_specular1 = F_lambert(Theta_array, AB, Rp2Rs, inc)
+    F_thermal2 = F_thermal(Theta_array, AB, F, Tss, Rp2Rs, 90)
+    F_specular2 = F_lambert(Theta_array, AB, Rp2Rs, 90)
+    F_Doppler1 = F_Doppler(Theta_array, alpha_Doppler)
+    F_ellip1 = F_ellip(Theta_array, alpha_ellipse)
     F_tr = F_Transit(Theta_array, Rp2Rs, *Coefficents, inc)
     print(f'time1: {time.time() - t1}')
     
@@ -51,14 +53,17 @@ if __name__ == '__main__':
     
     # subplot, plot each function as a subplot, so 4 subplots in total
     fig, axs = plt.subplots(2, 2, figsize=(10, 10))
-    axs[0, 0].plot(Theta_array, F_thermal)
+    axs[0, 0].plot(Theta_array, F_specular1/np.max(F_specular1), 'b')
+    axs[0, 0].plot(Theta_array, F_thermal1/np.max(F_thermal1), 'r')
+    axs[0, 0].plot(Theta_array, F_specular2/np.max(F_specular1), 'b--')
+    axs[0, 0].plot(Theta_array, F_thermal2/np.max(F_thermal1), 'r--')
     axs[0, 0].set_title("F_thermal")
-    axs[0, 0].set_ylim([min(F_thermal) *1.1, max(F_thermal)*1.2])
-    axs[0, 1].plot(Theta_array, F_specular)
+    # axs[0, 0].set_ylim([min(F_thermal) *1.1, max(F_thermal)*1.2])
+    axs[0, 1].plot(Theta_array, F_specular1)
     axs[0, 1].set_title("F_specular")
     axs[1, 0].plot(Theta_array, F_tr)
     axs[1, 0].set_title("F_Transit")
-    axs[1, 1].plot(Theta_array, F_ellip)
+    axs[1, 1].plot(Theta_array, F_ellip1)
     axs[1, 1].set_title("F_ellip")
     plt.show()
     
